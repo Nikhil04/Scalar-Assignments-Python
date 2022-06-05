577973b59ac229f6415f6ecd22380586d68036ae
Nikhil04 <nikhilpuri04@gmail.com>
2022-06-06 00:27:56 +0530

    updates
    updates

--- Python_Solution/Inversion Count of arr.py
+++ Python_Solution/Inversion Count of arr.py
@@ -1,6 +1,10 @@
 class Solution:
     # @param A : list of integers
     # @return an integer
+
+    def __init__(self) -> None:
+        pass
+        
     def merge(self,A, aux, low, mid, high):
         k = i = low
         j=mid+1
@@ -24,22 +28,23 @@ class Solution:
 
         
     def mergesort(self,A, aux, low, high):
-    	if high <= low:	
-    		return 0
-    	mid = low + ((high - low) >> 1)
-    	inversionCount = 0
-    	inversionCount += self.mergesort(A, aux, low, mid)  
-    	inversionCount += self.mergesort(A, aux, mid + 1, high)
-    	inversionCount += self.merge(A, aux, low, mid, high)
-    	return inversionCount%1000000007
+        if high <= low:	
+            return 0
+        mid = low + ((high - low) >> 1)
+        inversionCount = 0
+        inversionCount += self.mergesort(A, aux, low, mid)  
+        inversionCount += self.mergesort(A, aux, mid + 1, high)
+        inversionCount += self.merge(A, aux, low, mid, high)
+        return inversionCount%1000000007
 
     def solve(self, A):
         n=len(A)
         aux=A.copy()
         return (self.mergesort(A,aux,0,n-1))
 
-cat=Solution()
-arr=[3, 2, 1]
-count=cat.solve(arr)
-print("Inversion Count is:",count)
+if __name__ == "__main__":
+    cat=Solution()
+    arr=[3, 2, 1]
+    count=cat.solve(arr)
+    print("Inversion Count is:",count)
 
